4d942b710a5ee64af0e1bac4ca37d6a27e1b9a8f
Juan Andrey Malska <juan.malska@codeminer42.com>
2024-04-05 12:10:52 -0300

    views to visualize form answers and its details

--- spec/requests/form_answer_spec.rb
+++ spec/requests/form_answer_spec.rb
@@ -42,4 +42,64 @@ RSpec.describe 'FormAnswers', type: :request do
       expect(response).to redirect_to(root_url)
     end
   end
+
+  describe 'GET /answers' do
+    let!(:form) { create(:form) }
+    let!(:params) do
+      {
+        answers: form.fields.map do |form_field|
+          { field_id: form_field.id, answer: 'a' }
+        end
+      }
+    end
+    before do
+      post(answer_form_url(form.id), params:)
+    end
+
+    it 'returns correct http status' do
+      get form_answers_url(form.id)
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders correct template' do
+      get form_answers_url(form.id)
+      expect(response).to render_template(:answers)
+    end
+
+    it 'renders expected values' do
+      get form_answers_url(form.id)
+      expect(response.body).to include form.title.to_s
+      expect(response.body).to include form.form_answers.first.id.to_s
+    end
+  end
+
+  describe 'GET /detailed_answer' do
+    let!(:form) { create(:form) }
+    let!(:params) do
+      {
+        answers: form.fields.map do |form_field|
+          { field_id: form_field.id, answer: 'a' }
+        end
+      }
+    end
+    before do
+      post(answer_form_url(form.id), params:)
+    end
+
+    it 'returns correct http status' do
+      get form_answer_url(form.id, form.form_answers.first.id)
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders correct template' do
+      get form_answer_url(form.id, form.form_answers.first.id)
+      expect(response).to render_template(:detailed_answer)
+    end
+
+    it 'renders expected values' do
+      get form_answer_url(form.id, form.form_answers.first.id)
+      expect(response.body).to include form.title.to_s
+      expect(response.body).to include form.form_answers.first.form_field_answers.first.answer.to_s
+    end
+  end
 end
